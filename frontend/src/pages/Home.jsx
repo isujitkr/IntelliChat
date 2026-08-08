fb6d11ce0f36b5278e46390472350b874b701655
isujitkr <skr87981@gmail.com>
2026-08-09 00:30:03 +0530

    Login UI Created

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { auth, googleProvider } from "../utils/firebase";
+import { signInWithPopup } from "firebase/auth";
+import api from "../utils/axios";
+import { FcGoogle } from "react-icons/fc";
+
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Home = () => {
+
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (token) => {
+    try {
+      const { data } = await api.post("/auth/login", { token });
+
+      toast.success("Login successful!");
+
+    } catch (error) {
+      toast.error( 
+        error.response?.data?.message ||
+        error.response?.data?.error ||
+        "Login failed. Please try again."
+      )
+    }
+  };
+
+  const googleLogin = async () => {
+    if (loading) return;
+
+    try {
+      setLoading(true);
+
+      const result = await signInWithPopup(auth, googleProvider);
+
+      const token = await result.user.getIdToken();
+
+      await handleLogin(token);
+
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message ||
+        error.response?.data?.error ||
+        "Login failed. Please try again."
+      )
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className = "h-screen bg-black flex text-white overflow-hidden">
+        
+      <div className = "fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm">
+        <div className = "w-[340px] bg-[#13131c] border border-white/[0.08] rounded-2xl p-7 flex flex-col gap-5 ">
+            <div classsName = "flex flex-col gap-1">
+                <h2 clasName = "text-[17px] font-semibold text-slate-100 tracking-tight">Welcome to IntelliChat</h2>
+                <p className = " text-[13px] text-slate-500">Please login to continue using the app</p>
+            </div>
+            <button className = "w-full flex items-center justify-center gap-3 py-[11px] rounded-xl text-sm font-medium text-black/90 bg-white hover:bg-gray-200  transition-all duration-150 cursor-pointer" onClick = {googleLogin} disabled={loading}>
+                <FcGoogle size={15} />
+                Continue with Google
+            </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
