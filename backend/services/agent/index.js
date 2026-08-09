c602b0318d3abe0d8d79344109adf90a25cd0a29
isujitkr <skr87981@gmail.com>
2026-08-09 23:11:45 +0530

    Agent service created

---
+++ backend/services/agent/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
+
+const PORT = process.env.PORT || 8002;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Agent is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Agent service is running on port ${PORT}`);
+});
+
+connectDB();
