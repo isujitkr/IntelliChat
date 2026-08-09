af3366af1938fc49e670899662451e16c700f72d
isujitkr <skr87981@gmail.com>
2026-08-09 23:49:30 +0530

    Langgraph workflow diagram created

---
+++ backend/services/agent/graph/graph.js
@@ -0,0 +1,59 @@
+import { StateGraph } from "@langchain/langgraph";
+import { agentState } from "./state.js";
+import { router } from "./router.js";
+import { chatAgent } from "../agents/chat.agent.js";
+import { searchAgent } from "../agents/search.agent.js";
+import { codingAgent } from "../agents/coding.agent.js";
+import { pdfAgent } from "../agents/pdf.agent.js";
+import { pptAgent } from "../agents/ppt.agent.js";
+import { visionAgent } from "../agents/vision.agent.js";
+
+
+const workFlow = new StateGraph(agentState)
+
+workFlow.addNode("router", router);
+workFlow.addNode("chat", chatAgent);
+workFlow.addNode("search", searchAgent);
+workFlow.addNode("coding", codingAgent);
+workFlow.addNode("pdf", pdfAgent);
+workFlow.addNode("ppt", pptAgent);
+workFlow.addNode("vision", visionAgent);
+
+workFlow.addEdge("__start__", "router");
+
+workFlow.addConditionalEdges("router", (state) => {
+    switch (state.agent) {
+        case "chat":
+            return "chat";
+        case "search":
+            return "search";
+        case "coding":
+            return "coding";
+        case "pdf":
+            return "pdf";
+        case "ppt":
+            return "ppt";
+        case "vision":
+            return "vision";
+    
+        default:
+            return "char=t";
+    }
+},{
+    chat: "chat",
+    search: "search",
+    coding: "coding",
+    pdf: "pdf",
+    ppt: "ppt",
+    vision: "vision"
+});
+
+workFlow.addNode("search", "chat");
+
+workFlow.addEdge("chat", "__end__");
+workFlow.addEdge("coding", "__end__");
+workFlow.addEdge("pdf", "__end__");
+workFlow.addEdge("ppt", "__end__");
+workFlow.addEdge("vision", "__end__");
+
+export const graph = workFlow.compile();
